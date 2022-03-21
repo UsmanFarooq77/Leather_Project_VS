3d1e675d9693d4d38c9294c48ba6eb645b05a039
UsmanFarooq77 <usmanfarooq7721@gmail.com>
2022-03-22 04:41:30 +0500

    Add css on scrollbar and install scrollbar package for scroll.

--- src/app/@orders/orders.module.ts
+++ src/app/@orders/orders.module.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { OrdersRoutingModule } from './orders-routing.module';
 
-import { TypingAnimationDirective } from 'angular-typing-animation';
 import { TypingAnimationModule } from "angular-typing-animation";
 
 @NgModule({
