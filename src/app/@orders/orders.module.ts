b92ed74186f5dac5f58e83e7996dbf714c211145
UsmanFarooq77 <usmanfarooq7721@gmail.com>
2022-03-28 16:35:40 +0500

    Create setting module and component, config into core routing module.

--- src/app/@orders/orders.module.ts
+++ src/app/@orders/orders.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { MyOrdersComponent } from './my-orders/my-orders.component';
 import { OrdersRoutingModule } from './orders-routing.module';
 
-import { TypingAnimationModule } from "angular-typing-animation";
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    TypingAnimationModule,
+    SharedModule,
     OrdersRoutingModule
   ],
   declarations: [
---
+++ src/app/@settings/settings/settings.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
+
+@Component({
+  selector: 'app-settings',
+  templateUrl: './settings.component.html',
+  styleUrls: ['./settings.component.css']
+})
+export class SettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    $(document).ready(() => {
+      if ($('#date-countdown').length > 0) {
+        $('#date-countdown').countdown('2022/8/8', function (event) {
+          var $this = $(this).html(event.strftime(''
+            + '<span class="countdown-block"><span class="label">%w</span> weeks </span>'
+            + '<span class="countdown-block"><span class="label">%d</span> days </span>'
+            + '<span class="countdown-block"><span class="label">%H</span> hr </span>'
+            + '<span class="countdown-block"><span class="label">%M</span> min </span>'
+            + '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
+        });
+      }
+    })
+  }
+
+}
